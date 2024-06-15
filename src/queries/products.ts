9dc459114c64e002e39bc0276844ca005fec8e30
overdd <antonsiarko@gmail.com>
2024-06-15 22:16:16 +0200

    Revert "backend /products implementation added"
    This reverts commit 9cea2571376e887e7b1e14e5e8878f856abad8b1.

--- src/queries/products.ts
+++ src/queries/products.ts
@@ -9,7 +9,7 @@ export function useAvailableProducts() {
     "available-products",
     async () => {
       const res = await axios.get<AvailableProduct[]>(
-        `${API_PATHS.product}/products`
+        `${API_PATHS.bff}/product/available`
       );
       return res.data;
     }
@@ -29,7 +29,7 @@ export function useAvailableProduct(id?: string) {
     ["product", { id }],
     async () => {
       const res = await axios.get<AvailableProduct>(
-        `${API_PATHS.product}/products/${id}`
+        `${API_PATHS.bff}/product/${id}`
       );
       return res.data;
     },
