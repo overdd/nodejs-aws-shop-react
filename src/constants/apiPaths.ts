ecec993f11e4d70dfbae4368f370952f19009d1d
overdd <antonsiarko@gmail.com>
2024-06-29 21:06:22 +0200

    task 5 url update

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,7 @@
 const API_PATHS = {
-  product:
-    "https://9hf5z84ova.execute-api.us-east-1.amazonaws.com/prod",
+  product: "https://9hf5z84ova.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://vjmr32f6m2.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
